8206fda81d726a3a79cbe617cd7414fa41bcace2
AdamBundschuh <bundschuh.adam@gmail.com>
2023-05-09 09:12:48 -0400

    Excercise 01

---
+++ Day 10 Calculator/scratchpad.py
@@ -0,0 +1,15 @@
+# Docstring example
+
+def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    to return the title case version of the name."""
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return f"Hello, {first_name} {last_name}!"
+
+
+print(format_name("AdAm","bUnDsChUh"))
+
+
+
+
