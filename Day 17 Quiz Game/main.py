5d2b3f460abcc5727f42501c49bd3fc84bb0d570
AdamBundschuh <bundschuh.adam@gmail.com>
2023-05-22 14:52:04 -0400

    Day 17 Quiz Brain with OOP

---
+++ Day 17 Quiz Game/main.py
@@ -0,0 +1,18 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for data in question_data:
+    q_text = data["text"]
+    q_answer = data["answer"]
+    new_question = Question(q_text, q_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print(f"You completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
