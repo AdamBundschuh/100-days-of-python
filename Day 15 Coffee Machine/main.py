7fbcb5223559adc6961cdbf0e03fc9919e4cba2e
AdamBundschuh <bundschuh.adam@gmail.com>
2023-05-22 09:39:40 -0400

    Day 15 final version

--- Day 15 Coffee Machine/main.py
+++ Day 15 Coffee Machine/main.py
@@ -38,12 +38,14 @@ def check_resources(item):
     current_coffee = resources['coffee']
     current_milk = resources['milk']
 
+    item = MENU[item]
+
     water_needed = item['ingredients']['water']
     if current_water < water_needed:
         print("Not enough water.")
         return False
 
-    coffee_needed = item['ingredients']['water']
+    coffee_needed = item['ingredients']['coffee']
     if current_coffee < coffee_needed:
         print("Not enough coffee.")
         return False
@@ -58,6 +60,7 @@ def check_resources(item):
 
 
 def insert_coins(item):
+    """Takes number of coins as input from the user and totals the amount"""
     print("Please insert coins.")
 
     coins = {'quarters': {'value': 0.25,
@@ -78,53 +81,38 @@ def insert_coins(item):
     for coin in coins:
         value = coins[coin]['value']
         amount = coins[coin]['amount']
-        coins_inserted += value * amount
+        coins_inserted += (value * amount)
 
-    total_coins_inserted = round_money(coins_inserted)
-    print(f"Money inserted: ${total_coins_inserted}")
+    total_money = coins_inserted
+    item = MENU[item]
+    money_required = item['cost']
+    is_enough_money = total_money >= money_required
 
-    money_required = round_money(item['cost'])
-    print(f"Money required: ${money_required}")
+    if is_enough_money:
+        total_money = round_money(float(total_money) - float(money_required))
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
 
-    is_enough_money = total_coins_inserted >= money_required
-
-    return total_coins_inserted, is_enough_money
-
-
-def check_money(item):
-    print(item)
+    return total_money, is_enough_money
 
 
 def round_money(amount):
-    """Takes a number and returns a 2 decimal float with trailing zeros."""
-    return '{:.2f}'.format(round(amount, 2))
+    """Takes a number and returns a 2 decimal float including trailing zeros."""
+    return '{:.2f}'.format(float(round(amount, 2)))
 
 
-def make_espresso():
-    water_needed = MENU['espresso']['ingredients']['water']
-    coffee_needed = MENU['espresso']['ingredients']['water']
+def dispense_drink(drink_to_make):
+    drink_to_make = MENU[drink_to_make]
 
-    current_water = resources['water']
-    current_coffee = resources['coffee']
-
-    if current_water < water_needed:
-        return
-
-    money_needed = MENU['espresso']['cost']
-    if money < money_needed:
-        print("Not enough money.")
-        return
+    water_needed = drink_to_make['ingredients']['water']
+    coffee_needed = drink_to_make['ingredients']['coffee']
 
     resources['water'] -= water_needed
     resources['coffee'] -= coffee_needed
 
-
-def latte():
-    print("Make latte")
-
-
-def cappuccino():
-    print("Make cappuccino")
+    if "milk" in drink_to_make:
+        milk_needed = drink_to_make['ingredients']['milk']
+        resources['milk'] -= milk_needed
 
 
 def report():
@@ -133,15 +121,14 @@ def report():
     print(f"Money: ${money}")
 
 
-money = 500
+money = 0
 machine_on = True
 enough_resources = False
 enough_money = False
-money_inserted = 0
 
 while machine_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    drink = {}
+    drink_choice = {}
     make_drink = False
 
     if choice == "report":
@@ -150,16 +137,23 @@ while machine_on:
         print("Coffee machine powering off.")
         machine_on = False
     elif choice == "espresso":
-        drink = MENU["espresso"]
+        drink_choice = "espresso"
         make_drink = True
     elif choice == "latte":
-        latte()
+        drink_choice = "latte"
+        make_drink = True
     elif choice == "cappuccino":
-        cappuccino()
+        drink_choice = "cappuccino"
+        make_drink = True
     else:
         print("Invalid choice.")
 
     if make_drink:
-        enough_resources = check_resources(drink)
-        money_inserted, enough_money = insert_coins(drink)
+        enough_resources = check_resources(drink_choice)
+        money, enough_money = insert_coins(drink_choice)
 
+    if enough_resources and enough_money:
+        dispense_drink(drink_choice)
+        print(f'Here is ${money} in change.')
+        print(f'Here is your {drink_choice} ☕. Enjoy!')
+        money = 0
