091cc808de80223791e2fc8867e4818d0c3b6cfc
AdamBundschuh <bundschuh.adam@gmail.com>
2023-05-23 11:08:59 -0400

    Refactoring

--- Day 18 Turtle and GUI/day-18-scratchpad.py
+++ Day 18 Turtle and GUI/day-18-scratchpad.py
@@ -1,27 +1,7 @@
 import turtle
 from turtle import Turtle, Screen
+from colors import color_list
 import random
-
-# import colorgram
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     color_tuple = (r, g, b)
-#     rgb_colors.append(color_tuple)
-# print(rgb_colors)
-
-# 10 x 10
-# 20 diameter
-# 50 space between
-
-color_list = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
-              (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70),
-              (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74),
-              (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153),
-              (176, 192, 208), (168, 99, 102)]
 
 tim = Turtle()
 tim.hideturtle()
@@ -34,6 +14,7 @@ tim.sety(-300)
 
 
 def random_color():
+    """Returns a random rgb color in tuple format"""
     color_tuple = random.choice(color_list)
     return color_tuple
 
@@ -47,13 +28,11 @@ def new_row():
 
 def draw_row():
     """Draws a row of ten dots"""
-    print(f'Position before row loop: {tim.position()}')
     for _ in range(10):
         tim.pendown()
         tim.dot(20, random_color())
         tim.penup()
         tim.forward(50)
-    print(f'Position after row loop: {tim.position()}')
 
 
 for _ in range(10):
