547777491b75bf88679a10c497ffb58db6eea70f
AdamBundschuh <bundschuh.adam@gmail.com>
2023-05-31 13:55:07 -0400

    Day 24 Mail Merger

--- Day 24 Files and Directories/main.py
+++ Day 24 Files and Directories/main.py
@@ -7,18 +7,18 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-template = "./Input/Letters/starting_letter.txt"
-save_path = "./Output/ReadyToSend/"
+TEMPLATE_PATH = "./Input/Letters/starting_letter.txt"
+SAVE_PATH = "./Output/ReadyToSend/"
 
 with open("./Input/Names/invited_names.txt", "r") as data:
     names = data.readlines()
 
 for name in names:
     recipient_name = name.strip()
-    with open(template, "r") as data:
+    with open(TEMPLATE_PATH, "r") as data:
         new_letter = data.read().replace("[name]", recipient_name)
 
     file_name = "letter_to_" + recipient_name.replace(" ", "_") + ".txt"
-    with open(save_path + file_name, mode="w") as data:
+    with open(SAVE_PATH + file_name, mode="w") as data:
         data.write(new_letter)
 
