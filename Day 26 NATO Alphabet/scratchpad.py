9c279e22c785f5e36d3f690b5728c367516628df
AdamBundschuh <bundschuh.adam@gmail.com>
2023-06-05 14:40:14 -0400

    Day 26 Exercise 04

--- Day 26 NATO Alphabet/scratchpad.py
+++ Day 26 NATO Alphabet/scratchpad.py
@@ -1,3 +1,5 @@
+from random import randint
+
 numbers = [1, 2, 3]
 # new_list = [new_item for item in list]
 new_list = [n + 1 for n in numbers]
@@ -16,3 +18,11 @@ print(short_names)
 
 uppercase_names = [name.upper() for name in names if len(name) > 4]
 print(uppercase_names)
+
+names = ['Adam', 'Dave', 'Alex', 'Serena', 'Devil Woman', 'Caroline', 'Bethany']
+# new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+student_scores = {student:randint(1, 100) for student in names}
+print(student_scores)
+
+passed_students = {student:score for (student,score) in student_scores.items() if score >= 60}
+print(passed_students)
