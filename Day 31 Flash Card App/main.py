11cc0084200c1f156d545b21287dfdd02d8d8470
AdamBundschuh <bundschuh.adam@gmail.com>
2023-06-07 14:55:48 -0400

    Day 31 Flash Card App Refactored text to canvas from Label

--- Day 31 Flash Card App/main.py
+++ Day 31 Flash Card App/main.py
@@ -6,19 +6,16 @@ import json
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
-TOP_TXT_SIZE = 30
-BOT_TXT_SIZE = 50
+TOP_TXT_SIZE = 40
+BOT_TXT_SIZE = 60
 TXT_ITALIC = "italic"
 TXT_BOLD = "bold"
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Flash Card App")
+window.title("Flashy McFlasherson")
 window.resizable(width=False, height=False)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-# Card
-canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 
 # Images
 card_front = PhotoImage(file="./images/card_front.png")
@@ -26,7 +23,11 @@ card_back = PhotoImage(file="./images/card_back.png")
 right = PhotoImage(file="./images/right.png")
 wrong = PhotoImage(file="./images/wrong.png")
 
+# Card
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=(FONT_NAME, TOP_TXT_SIZE, TXT_ITALIC))
+canvas.create_text(400, 263, text="word", font=(FONT_NAME, BOT_TXT_SIZE, TXT_BOLD))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Buttons
@@ -38,11 +39,6 @@ right_btn = Button(image=right)
 right_btn.config(highlightthickness=0, bd=0, relief="flat")
 right_btn.grid(column=1, row=1, pady=(30, 0))
 
-# Text
-top_title = Label(text="French", font=(FONT_NAME, TOP_TXT_SIZE, TXT_ITALIC))
-top_title.grid(column=0, row=0, columnspan=2, pady=(0, 150))
 
-bot_title = Label(text="croissant", font=(FONT_NAME, BOT_TXT_SIZE, TXT_BOLD))
-bot_title.grid(column=0, row=0, columnspan=2, pady=(150, 0))
 
 window.mainloop()
