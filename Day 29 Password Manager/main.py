48dee59981b74770fdb78a2d590c36b6c266e66e
AdamBundschuh <bundschuh.adam@gmail.com>
2023-06-07 14:10:23 -0400

    Day 30 Added search functionality to Password Manager

--- Day 29 Password Manager/main.py
+++ Day 29 Password Manager/main.py
@@ -37,7 +37,7 @@ def generate_password():
 
     pw_copied_lbl = Label(text="Password Copied to Clipboard")
     pw_copied_lbl.config(highlightbackground="green", fg="green", borderwidth=0, bg=LIGHT_GREY)
-    pw_copied_lbl.place(x=155, y=190)
+    pw_copied_lbl.place(x=175, y=190)
     window.after(3500, pw_copied_lbl.destroy)
 
 
@@ -45,6 +45,7 @@ def generate_password():
 def save_json_to_file(data):
     with open("data.json", mode="w") as data_file:
         json.dump(data, data_file, indent=4)
+
 
 def save():
     website = website_entry.get()
@@ -76,11 +77,34 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH WEBSITES ------------------------------- #
+def find_password():
+    searched_website = website_entry.get()
+
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Oopsy Poopsie!",
+                             message="No Data File Found.\nPlease add a website/password first.")
+    else:
+        if searched_website in data:
+            email = data[searched_website]["email"]
+            password = data[searched_website]["password"]
+            messagebox.showinfo(title=searched_website + " Information",
+                                message=f"Email: {email}\n"
+                                        f"Password: {password}\n\nPassword has been copied to the clipboard.")
+            pyperclip.copy(password)
+        else:
+            messagebox.showerror(title="Oh no!", message="No details for the website exists.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.resizable(width=False, height=False)
 window.config(padx=30, pady=30, bg=LIGHT_GREY)
+window.iconbitmap("logo_s.ico")
 
 # Logo
 canvas = Canvas(width=200, height=200, bg=LIGHT_GREY, highlightthickness=0)
@@ -92,17 +116,19 @@ canvas.grid(column=1, row=0)
 website_lbl = Label(text="Website:", font=(FONT_NAME, FONT_SIZE, FONT_BOLD), bg=LIGHT_GREY)
 website_lbl.grid(column=0, row=1, stick="E", padx=(0, 10), pady=(25, 0))
 
-website_entry = Entry(width=35, font=(FONT_NAME, FONT_SIZE))
-website_entry.grid(column=1, row=1, columnspan=2, sticky="EW", pady=(25, 0))
-# website_entry.insert(END, "Facebook")
+website_entry = Entry(width=21, font=(FONT_NAME, FONT_SIZE))
+website_entry.grid(column=1, row=1, sticky="EW", pady=(25, 0), padx=(0, 10))
 website_entry.focus()
+
+search_btn = Button(text="Search", command=find_password)
+search_btn.grid(column=2, row=1, sticky="EW", pady=(25, 0))
 
 # Email/User Row
 email_lbl = Label(text="Email/Username:", font=(FONT_NAME, FONT_SIZE, FONT_BOLD), bg=LIGHT_GREY)
 email_lbl.grid(column=0, row=2, padx=(0, 10))
 
 email_entry = Entry(width=35, font=(FONT_NAME, FONT_SIZE))
-email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+email_entry.grid(column=1, row=2, columnspan=2, sticky="EW", pady=(5, 0))
 email_entry.insert(END, "bundschuh.adam@gmail.com")
 
 # Password Row
@@ -111,7 +137,6 @@ password_lbl.grid(column=0, row=3, stick="E", padx=(0, 10))
 
 password_entry = Entry(width=21, font=(FONT_NAME, FONT_SIZE))
 password_entry.grid(column=1, row=3, sticky="EW", padx=(0, 10))
-# password_entry.insert(END, "123456789")
 
 password_btn = Button(text="Generate Password", command=generate_password)
 password_btn.grid(column=2, row=3, sticky="EW")
